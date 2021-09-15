df79f338d5b72fdfe0bbc3ef943ac8422d3bc0ca
Ashutosh <ashutosh.shukla@innovect.com>
2021-09-16 00:17:07 +0530

    1.Added swagger request validator. 2.jayway.jsonpath added.

---
+++ src/main/java/org/ashu/schema/validation/GenericRequestValidator.java
@@ -0,0 +1,7 @@
+package org.ashu.schema.validation;
+
+import com.atlassian.oai.validator.interaction.response.CustomResponseValidator;
+
+public interface GenericRequestValidator extends CustomResponseValidator{
+
+}
